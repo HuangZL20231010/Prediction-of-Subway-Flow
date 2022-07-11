a5cbfbda3b1d9ea4b42c0b0b8f13466f8b05c95d
Wang Jiong <911124552@qq.com>
2022-07-11 13:39:36 +0800

    2022/7/11 wj 13:38 bug修复

--- springboot/src/main/java/com/example/demo/util/TimeUtil.java
+++ springboot/src/main/java/com/example/demo/util/TimeUtil.java
@@ -12,30 +12,27 @@ public class TimeUtil
         this.time = time;
     }
     // 该方法将自身的time设置为临近的能够查表的time并返回是否成功
-    // 例如：传入的time是2022/7/7 18:07:00,就需要转换成2022/7/7 18:10:00,同理18:03转换成18:00
+    // 例如：传入的time是2022/07/07 18:07:00,就需要转换成2022/7/7 18:10:00,同理18:03转换成18:00
     public boolean toApproachTime()
     {
 
-        char[] t = time.toCharArray();
-        char a=t[t.length-4];
-        char b=t[t.length-5];
-//        if (a >= '5'&&b<'5') {
-//            t[t.length - 5] = (char) (t[t.length - 5] + 1);
-//        }
-//        else if (a >= '5'&&b>='5')
-//        {
-//            t[t.length - 7] = (char) (t[t.length - 7] + 1);
-//            t[t.length - 5] ='0';
-//        }
-//        else
-//        {
-//
-//        }
-        t[t.length-4]='0';
-        t[t.length-3]=':';
-        t[t.length-2]='0';
-        t[t.length-1]='0';
-        time=new String(t);
+        String HH = time.substring(11, 13);
+        String MM;
+
+        int M = Integer.parseInt(time.substring(14, 16)) / 10 * 10;
+
+        if (M < 10)
+        {
+            MM = "0" + String.valueOf(M);
+        }
+        else
+        {
+            MM = String.valueOf(M);
+        }
+
+        time = "2015/04/29 " + HH + ":" + MM + ":00";
+
+
         return true;
     }
 }
