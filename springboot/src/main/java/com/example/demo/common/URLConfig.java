394e67f4e0e38783b9fa8b749ca35447a6c52fe0
Wang Jiong <911124552@qq.com>
2022-07-12 20:10:49 +0800

    2022/7/12 wj 20:10 前后端文件映射测试

---
+++ springboot/src/main/java/com/example/demo/common/URLConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 图片绝对地址与虚拟地址映射
+ */
+
+@Configuration
+public class URLConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //文件磁盘图片url 映射
+        //配置server虚拟路径，handler为前台访问的目录，locations为files相对应的本地路径
+        registry.addResourceHandler("/pic/view/**").addResourceLocations("file:/E:/pic/");
+        registry.addResourceHandler("/video/view/**").addResourceLocations("file:/E:/video/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+}
