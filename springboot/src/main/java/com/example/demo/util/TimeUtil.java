0c70353960585c9a5a521c5aab0236ebbc692dfb
xixilili423 <1782532694@qq.com>
2022-07-13 10:07:33 +0800

    早晚高峰

--- springboot/src/main/java/com/example/demo/util/TimeUtil.java
+++ springboot/src/main/java/com/example/demo/util/TimeUtil.java
@@ -41,6 +41,34 @@ public class TimeUtil
     // 如果时间位于7:30至9:30返回1，位于17:30至19:30返回2，其他时间点返回3
     public Integer judgeTimePeriod(String time)
     {
-        return 0;
+        String HH = time.substring(11, 13);
+        int H=Integer.parseInt(HH);
+        int M = Integer.parseInt(time.substring(14, 16)) / 10 * 10;
+        if(H>=7&&H<=9)
+        {
+            if(H==7&&M<30||H==9&&M>30)
+            {
+                return 3;
+            }
+            else
+            {
+                return 1;
+            }
+
+        } else if (H>=17&&H<=19) {
+            if(H==17&&M<30||H==19&&M>30)
+            {
+                return 3;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+        else
+        {
+            return 3;
+        }
+
     }
 }
